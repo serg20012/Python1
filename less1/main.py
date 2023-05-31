2d8cd7c7be49e0ac6651868ff255bac90d3ce2a2
serg <123456st@mail.ru>
2023-05-31 17:22:43 +0800

    3 home work

---
+++ less1/main.py
@@ -0,0 +1,77 @@
+# print(5, 8, 6)
+# print(5, 8, 6)
+# print(5, 8, 6)
+# print(5, 8, 6)
+# print(5, 8, 6)
+# print(5, 8, 6)
+# n = 5
+# print(type(n))
+# n = 'bbb"rrr5"'
+# print(type(n))
+# print(n)
+# print("ddtlbnt cnhjre")
+# a = input()
+# print(a)
+# b = input("ddtlbnt второе число")
+# print(a+b)
+
+# c = 5.86
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c)
+# print(type(c))
+
+# print("введите число")
+# a = int(input())
+# b = int(input("введите второе число "))
+# print(f"{a} + {b} =", a+b)
+
+# a = 5.89566
+# b = 6.54646
+# print(round(a*b, 3))
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 < 3 < 5 < 10
+# print(a)
+# -----------------------
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+# -------------------------
+# r = range(100, 0, -20)
+# for i in r:
+#     print(i)
+# --------------------------
+# a = "qwertty"
+# for i in a:
+#     print(i)
+# ----------------------------
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "+"
+#     print(line)
+# ----------------------
+# text = "Сьешь еще ЭтиХ БулоЧЕк"
+# print(len(text))
+# print("еще" in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace("еще", "ЕЩЕ"))
+# --------------------------
+# text = "Сьешь еще ЭтиХ БулоЧЕк"
+# print(text[:2])
+# print(text[2:9])
+# print(text[0:len(text):6])
+# print(text[::6])
---
+++ less2/main.py
@@ -0,0 +1,33 @@
+# # list_1 = []
+# # list_1 = list()
+# list_1 = [1, 2, 3, 8]
+# # print(len(list_1))
+# print(list_1)
+
+# list_1.append(8)
+# print(list_1)
+# a = list_1.pop()
+# print(list_1)
+# print(a)
+# # Добавление элемента
+# list_1.insert(2, 55)
+# print(list_1)
+# -----------------кортеж
+# v = [1, 2, 3, 4]
+# print(type(v))
+# v = tuple(v)
+# print(type(v))
+
+# a, b, c = v
+# print(a, b, c)
+# ------------------словарь
+# d = {}
+# d["q"] = "qwer"
+# print(d)
+dicty = {"a": "up", "b": "left"}
+# print(dicty)
+# # del dicty["a"]
+# print(dicty)
+
+print("{}:{}".format("b", dicty["b"]))
+dicty.items
